573cb08d5066a28d1821335efef85030b7ff2417
Owen Kelly <owen@owenkelly.com.au>
2019-02-07 22:50:46 +1100

    added feature flags

---
+++ packages/kerosene-feature-flags/src/index.ts
@@ -0,0 +1,28 @@
+import { Condition, Runtime } from "./Condition";
+
+type Flag = (runtime: Runtime) => boolean;
+
+type FlagCallback = (c: Condition) => Condition[];
+
+// flag constructor, creates a flag that can be executed at runtime
+// to determine it's status
+function flag(callback: FlagCallback): Flag {
+    const condition = new Condition();
+    const checks = callback(condition);
+
+    // Return a function to be called at runtime, to check if the flag
+    // is enabled
+    return (runtime: any): boolean => {
+        return (
+            checks
+                .map(check => {
+                    // Check the assertion is valid
+                    return check.validate(runtime);
+                })
+                // check every assertion is valid with a result of true
+                .every(result => result === true)
+        );
+    };
+}
+
+export { flag, Flag };
