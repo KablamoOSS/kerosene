9d205f4c2795aea42dfc68c23f8dc411ff5a3544
Bryce Hanscomb <bryce@brycehanscomb.com>
2019-02-27 09:31:52 +1100

    chore: apply code style

--- packages/kerosene/src/array/toggle.spec.ts
+++ packages/kerosene/src/array/toggle.spec.ts
@@ -1,54 +1,54 @@
 import toggle from "./toggle";
 
-describe('#toggle', () => {
+describe("#toggle", () => {
 
-  describe('should return an array containing the original array items...', () => {
+  describe("should return an array containing the original array items...", () => {
 
-    describe('plus the needle if the needle is not currently in haystack', () => {
+    describe("plus the needle if the needle is not currently in haystack", () => {
 
-      it('and the haystack is empty', () => {
+      it("and the haystack is empty", () => {
         const haystack = [] as number[];
         const needle = 4;
 
         expect(
-            toggle(needle, haystack)
+            toggle(needle, haystack),
         ).toEqual(
-            [...haystack, needle]
+            [...haystack, needle],
         );
       });
 
-      it('and the haystack is not empty', () => {
-        const haystack = [1,2,3];
+      it("and the haystack is not empty", () => {
+        const haystack = [1, 2, 3];
         const needle = 4;
 
         expect(
-            toggle(needle, haystack)
+            toggle(needle, haystack),
         ).toEqual(
-            [...haystack, needle]
+            [...haystack, needle],
         );
       });
 
     });
 
-    it('without the needle if the needle is currently in haystack, once', () => {
-      const haystack = [1,2,3];
+    it("without the needle if the needle is currently in haystack, once", () => {
+      const haystack = [1, 2, 3];
       const needle = 2;
 
       expect(
-          toggle(needle, haystack)
+          toggle(needle, haystack),
       ).toEqual(
-          [1,3]
+          [1, 3],
       );
     });
 
-    it('without the needle if the needle is currently in haystack, multiple times', () => {
-      const haystack = [1,4,2,3,4,4];
+    it("without the needle if the needle is currently in haystack, multiple times", () => {
+      const haystack = [1, 4, 2, 3, 4, 4];
       const needle = 4;
 
       expect(
-          toggle(needle, haystack)
+          toggle(needle, haystack),
       ).toEqual(
-          [1,2,3]
+          [1, 2, 3],
       );
     });
 
