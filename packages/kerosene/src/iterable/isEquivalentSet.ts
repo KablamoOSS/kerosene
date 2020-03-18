ac00db77a8e69a95ac55d39ca423b2c81b40ebf6
Nathan Hardy <nhardy@users.noreply.github.com>
2020-03-18 12:50:01 +1100

    kerosene: Add isEquivalentSet method (#62)
    * kerosene: Add isEquivalentSet method
    
    * Bump version
    
    Co-authored-by: Owen Kelly <owen.kelly@kablamo.com.au>

---
+++ packages/kerosene/src/iterable/isEquivalentSet.ts
@@ -0,0 +1,10 @@
+import { isEqual } from "lodash";
+
+/**
+ * Returns whether `iterable1` is an equivalent set to `iterable2`
+ * @param iterable1
+ * @param iterable2
+ */
+export default function isEquivalentSet<T>(iterable1: Iterable<T>, iterable2: Iterable<T>): boolean {
+  return iterable1 === iterable2 || isEqual(new Set(iterable1), new Set(iterable2));
+}
