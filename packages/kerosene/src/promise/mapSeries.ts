280d92d876513b87f01684367bcb257c60dbcbf3
Nathan Hardy <nhardy@users.noreply.github.com>
2022-04-01 15:09:19 +1100

    kerosene: Add mapSeries promise util (#82)
    * kerosene: Add Promise mapSeries util
    
    * Bump versions
    
    * Fix @sinonjs/fake-timers import
    
    * Fix coverage

---
+++ packages/kerosene/src/promise/mapSeries.ts
@@ -0,0 +1,26 @@
+/* eslint-disable @typescript-eslint/ban-types */
+
+/**
+ * Maps over `items` with a promise-returning `iteratee` in series
+ * @param items
+ * @param iteratee
+ */
+export default function mapSeries<TItems extends any[], U>(
+  items: readonly [...TItems],
+  iteratee: (
+    value: TItems[number],
+    index: number,
+    array: Readonly<TItems>,
+  ) => Promise<U>,
+): Promise<{ [Index in keyof TItems]: U }> {
+  return items.reduce(
+    (acc, item, index, array) =>
+      acc.then((previous) =>
+        iteratee(item, index, array as TItems).then((result) => [
+          ...previous,
+          result,
+        ]),
+      ),
+    Promise.resolve([] as U[] as { [Index in keyof TItems]: U }),
+  );
+}
