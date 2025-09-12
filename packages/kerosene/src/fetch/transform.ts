0347ef3be0752bb595e1a238732fe1038636b530
Aaron Clover <aaron.clover@kablamo.com.au>
2025-09-12 14:36:21 +1000

    Checking new transform setup

--- packages/kerosene/src/fetch/transform.ts
+++ packages/kerosene/src/fetch/transform.ts
@@ -1,31 +1,47 @@
 import { parse } from "content-type";
 
+export type CreateTransformOptions = {
+  defaultTransform?: (res: Response) => Promise<unknown>;
+};
+
 /**
  * Takes a fetch response and attempts to transform the response automatically according to the status code and
  * Content-Type header (if provided)
  * @param response
  */
-export default function transform(response: Response) {
-  if (response.status === 204) {
-    return Promise.resolve(null);
-  }
+export function createTransform({
+  defaultTransform,
+}: CreateTransformOptions = {}) {
+  return (response: Response) => {
+    if (response.status === 204) {
+      return Promise.resolve(null);
+    }
 
-  const contentType = response.headers.get("Content-Type");
+    const contentType = response.headers.get("Content-Type");
 
-  if (!contentType) {
-    return response.text();
-  }
+    if (!contentType) {
+      return defaultTransform ? defaultTransform(response) : response.text();
+    }
 
-  const { type } = parse(contentType);
+    const { type } = parse(contentType);
 
-  switch (type) {
-    case "application/json":
-      return response.json();
+    switch (type) {
+      case "application/json":
+        return response.json();
 
-    case "text/plain":
-      return response.text();
+      case "text/plain":
+        return response.text();
 
-    default:
-      return response.blob();
-  }
+      default:
+        return defaultTransform ? defaultTransform(response) : response.blob();
+    }
+  };
 }
+
+export const transformDefaultJson = createTransform({
+  defaultTransform: (response) => response.json(),
+});
+
+const transform = createTransform();
+
+export default transform;
