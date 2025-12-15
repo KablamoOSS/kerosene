6127f2353a33def410f4daae41cb10c8bccbf654
Nathan Hardy <nhardy@users.noreply.github.com>
2025-12-15 17:30:27 +1100

    Re-add @kablamo/rollup-plugin-resolve-externals to resolve externals for .mjs

--- packages/kerosene-ui/tsdown.config.ts
+++ packages/kerosene-ui/tsdown.config.ts
@@ -1,12 +1,24 @@
+import resolveExternals from "@kablamo/rollup-plugin-resolve-externals";
 import { optimizeLodashImports } from "@optimize-lodash/rollup-plugin";
 import { defineConfig } from "tsdown";
+import packageJson from "./package.json" with { type: "json" };
 
 export default defineConfig({
   dts: true,
   entry: ["src/index.ts", "src/react-query.ts"],
   // esm type exports aren't set correctly
   // exports: true,
+  // include everything, as resolveExternals() will handle this instead
+  noExternal: /.*?/,
   format: ["cjs", "esm"],
-  plugins: [optimizeLodashImports({ parseOptions: { lang: "ts" } })],
+  plugins: [
+    optimizeLodashImports({ parseOptions: { lang: "ts" } }),
+    resolveExternals({
+      externals: [
+        packageJson.dependencies,
+        packageJson.peerDependencies,
+      ].flatMap(Object.keys),
+    }),
+  ],
   sourcemap: true,
 });
