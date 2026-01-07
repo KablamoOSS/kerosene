6717566b67a48fc29a404cb0685c053f88460f17
Nathan Hardy <nhardy@users.noreply.github.com>
2026-01-07 12:18:42 +1100

    rollup-plugin-resolve-externals: Disable during declaration emit, handle MODULE_NOT_FOUND (#165)

--- packages/rollup-plugin-resolve-externals/src/index.ts
+++ packages/rollup-plugin-resolve-externals/src/index.ts
@@ -29,44 +29,53 @@ export default function resolveExternals({
   return {
     name: "@kablamo/rollup-plugin-resolve-externals",
 
-    resolveId(source) {
+    resolveId(source, importer) {
       // Ignore non-externals
       if (!external(source)) return null;
+
+      // Ignore during TypeScript declaration emit
+      if (importer?.endsWith(".d.ts")) return null;
 
       // Match deep imports from a package
       const match = deepExternalRegex.exec(source);
       if (match) {
         const [, mod, importPath] = match;
-        // eslint-disable-next-line import/no-dynamic-require, global-require, @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
-        const pkgJson = require(`${mod}/package.json`) as Record<
-          string,
-          unknown
-        >;
+
+        const pkgJsonLocation = require.resolve(`${mod}/package.json`, {
+          paths,
+        });
+        // eslint-disable-next-line import/no-dynamic-require, global-require, @typescript-eslint/no-require-imports
+        const pkgJson = require(pkgJsonLocation) as Record<string, unknown>;
 
         // If package exports are not defined, we need to resolve the path so that CommonJS files
         // can be resolved properly from `.mjs` files
         if (!pkgJson.exports) {
-          // Resolve the module to a path on disk
-          const resolvedPath = require.resolve(`${mod}/${importPath}`, {
-            paths,
-          });
-          // Rewrite to a relative path
-          const rewritten = `${mod}/${path
-            .relative(
-              path.dirname(
-                require.resolve(`${mod}/package.json`, {
-                  paths,
-                }),
-              ),
-              resolvedPath,
-            )
-            // Handle win32 path separators
-            .split(path.sep)
-            .join("/")}`;
-          return {
-            id: rewritten,
-            external: true,
-          };
+          try {
+            // Resolve the module to a path on disk
+            const resolvedPath = require.resolve(`${mod}/${importPath}`, {
+              paths,
+            });
+            // Rewrite to a relative path
+            const rewritten = `${mod}/${path
+              .relative(path.dirname(pkgJsonLocation), resolvedPath)
+              // Handle win32 path separators
+              .split(path.sep)
+              .join("/")}`;
+            return {
+              id: rewritten,
+              external: true,
+            };
+          } catch (err) {
+            // If a module can't be resolved, leave its path unchanged. Otherwise, re-throw the error.
+            if (
+              !err ||
+              typeof err !== "object" ||
+              !("code" in err) ||
+              err.code !== "MODULE_NOT_FOUND"
+            ) {
+              throw err;
+            }
+          }
         }
       }
 
